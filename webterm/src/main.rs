d903fc78609debd47877be939feadc462358902d
orangatun <raghunadhan@protonmail.com>
2024-11-17 19:07:36 -0800

    Adds skeleton for commands
    Adds the skeleton code for the commands

--- webterm/src/main.rs
+++ webterm/src/main.rs
@@ -4,7 +4,7 @@ use dioxus::prelude::*;
 use dioxus_logger::tracing::{info, Level};
 use std::collections::HashMap;
 use chrono::Local;
-
+use std::sync::OnceLock;
 
 fn main() {
     // Init logger
@@ -13,13 +13,36 @@ fn main() {
     launch(app);
 }
 
+enum Errors {
+    CommandNotFound,
+    ExtraParametersPassed,
+    PathNotFound,
+    FileNotFound
+}
+
 struct TerminalEntryData {
     req: String,
-    resp: String
+    resp: Element
+}
+
+fn fs() -> &'static HashMap<&'static str, &'static [&'static str; 4]> {
+    static FS: OnceLock<HashMap<&str,   &'static [&'static str; 4]>> = OnceLock::new();
+    FS.get_or_init(|| {
+        let mut fs = HashMap::from([
+            ("/", &["about", "projects", "work", "random"]),
+            ("about/", &["summary", "/secrets", "", ""]),
+            ("about/secrets/", &["shhh", "", "", ""]),
+            ("projects/", &["summary", "", "", ""]),
+            ("random/", &["warp", "rules_of_internet", "", ""])
+        ]);
+        fs
+    })
 }
 
 #[component]
 fn app() -> Element {
+
+    let mut current_path =use_signal(|| HashMap::from([(0u8,"/".to_string())]));
     let mut entries = use_signal(HashMap::<u32, TerminalEntryData>::new);
 
     let mut start_from = use_signal(||0);
@@ -39,27 +62,115 @@ fn app() -> Element {
             ul {
                 class: "entries-list",
                 for id in start_from()..ending() {
-                    TerminalEntry { key: "{id}", id, entries}
+                    TerminalEntry { key: "{id}", id, entries, current_path}
                 }
             },
-            TerminalActiveEntry { entries , start_from, ending },
+            TerminalActiveEntry { entries , start_from, ending , current_path},
         }
     }
 }
 
-fn generate_response(req: String) -> String {
-    format!("Response to: {}", req)
+fn ls_response(words: Vec::<String>, current_path: Signal<HashMap::<u8, String>>) -> Element{
+    rsx! {
+        div {
+            "Directories and Files go here",
+        }
+    }
+}
+
+fn cat_dog_response(words: Vec::<String>) -> Element{
+    rsx! {
+        div {
+            "Cat/Dog response",
+        }
+    }
+}
+
+
+fn cd_response(words: Vec::<String>, mut current_path: Signal<HashMap::<u8, String>>) -> Element{
+    if words.len()==1 {
+        for i in 1..current_path().len() {
+            let key = i as u8;
+            current_path().remove(&key);
+        }
+    } else {
+        //Path check, and change
+    }
+    
+    rsx! {    
+        div {
+            "cd response here",
+        }
+    }
+}
+
+fn resolve_error(err: Errors) -> Element {
+    rsx! {
+        div {
+            "Error resolved here"
+        }
+    }
+}
+
+fn resolve_exit() -> Element {
+    rsx! {
+        link { rel: "stylesheet", href: "terminal_prompt.css" },
+        div { id: "exit-container",
+            p { "There's no exiting to this terminal." },
+            p { "The only way out is turning off the internet." }
+            p { class: "whisper-text", "Or, you could close this tab." }
+        }
+    }
+}
+
+fn generate_response(req: String, current_path: Signal<HashMap::<u8, String>>) -> Element {
+    let words: Vec<String> = req.trim().split_whitespace().map(|v| v.to_string()).collect();
+
+    if words.len()==0 {
+        return resolve_error(Errors::CommandNotFound);
+    } else {
+        let first_word = words.first().unwrap();
+        match first_word.as_str() {
+            "ls" => {
+                if words.len()>1 {
+                    return resolve_error(Errors::ExtraParametersPassed);
+                } else {
+                    return ls_response(words, current_path);
+                }
+            },
+            "cd" => {
+                return cd_response(words, current_path);
+            },
+            "pwd" => {
+                let p_len = (current_path().len() as u8)-1u8;
+                return rsx! {
+                        div {
+                            "The current working directory is: {current_path().get(&p_len).unwrap()}"
+                        }
+                    }
+            }, 
+            "cat" | "dog" => {
+                return cat_dog_response(words);
+            },
+            "exit" => {
+                return resolve_exit();
+            }
+            _ => {
+                return resolve_error(Errors::CommandNotFound);
+            }
+        }
+    }
 }
 
 #[component]
-fn TerminalActiveEntry(mut entries: Signal<HashMap<u32, TerminalEntryData>>, mut start_from: Signal<u32>, mut ending: Signal<u32>) -> Element {
+fn TerminalActiveEntry(mut entries: Signal<HashMap<u32, TerminalEntryData>>, mut start_from: Signal<u32>, mut ending: Signal<u32>, mut current_path: Signal<HashMap::<u8, String>>) -> Element {
     let mut draft = use_signal(|| "".to_string());
     let mut entry_id = use_signal(|| 0);
 
     let onkeyup = move |event: KeyboardEvent| {
         if event.key() == Key::Enter && !draft.read().is_empty() {
             let tmp_str = draft.to_string();
-            if(tmp_str=="clear") {
+            if tmp_str=="clear" {
                 entries.write().clear();
                 start_from-=start_from();
                 ending -=ending();
@@ -68,7 +179,7 @@ fn TerminalActiveEntry(mut entries: Signal<HashMap<u32, TerminalEntryData>>, mut
                 let id = entry_id();
                 let entry = TerminalEntryData {
                     req: draft.to_string(),
-                    resp: generate_response(draft.to_string()),
+                    resp: generate_response(draft.to_string(), current_path),
                 };
                 ending += 1;
                 entries.write().insert(id, entry);
@@ -93,7 +204,7 @@ fn TerminalActiveEntry(mut entries: Signal<HashMap<u32, TerminalEntryData>>, mut
 }
 
 #[component]
-fn TerminalEntry(mut entries: Signal<HashMap<u32, TerminalEntryData>>, id: u32) -> Element {
+fn TerminalEntry(mut entries: Signal<HashMap<u32, TerminalEntryData>>, id: u32, current_path: Signal<HashMap::<u8, String>>) -> Element {
     let req = entries.read().get(&id).unwrap().req.clone();
     let resp = entries.read().get(&id).unwrap().resp.clone();
 
@@ -107,10 +218,7 @@ fn TerminalEntry(mut entries: Signal<HashMap<u32, TerminalEntryData>>, id: u32) 
                 class:"req",
                 "{req}"
             },
-            p {
-                class:"resp",
-                "{resp}"
-            }
+            { resp }
         }
     }
 }
